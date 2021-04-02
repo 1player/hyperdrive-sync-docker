c64164c3e3faea27299599199f4e91b0002e326d
Stéphane Travostino <steph@combo.cc>
2021-04-02 11:36:39 +0100

    Save storage directory into config volume

--- start.sh
+++ start.sh
@@ -2,13 +2,20 @@
 
 set -euxo pipefail
 
+# Set up hyperspace storage directory
+if [ ! -e /config/hyperspace ]; then
+    mkdir /config/hyperspace
+fi
+
+ln -s /config/hyperspace $HOME/.hyperspace
+
 # Create a drive if it hasn't been created yet
 if [ ! -e /config/drive.id ]; then
     hyp create drive 2>&1 | sed -rne 's/Drive Created: (.*)/\1/ p' > /config/drive.id
 fi
 
 DRIVE_ID=$(cat /config/drive.id)
-hyp drive sync -w /data/ $DRIVE_ID/
+hyp drive sync --yes --watch /data/ $DRIVE_ID/
 
 
 
